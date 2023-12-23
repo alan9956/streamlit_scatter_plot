1d42377e1daf8973a9cee8b271b115805684eb70
alan9956 <alan9956@ydu.edu.tw>
2023-12-23 11:17:23 +0800

    Update app.py

--- streamlit_scatter_plot/app.py
+++ streamlit_scatter_plot/app.py
@@ -1,7 +1,6 @@
 """
-File     : hello_streamlit\app.py
-Title    : first streamlit app based R-shiny "01_hello"
-Date     : 2023.01.07
+File     : app.py
+Date     : 2023.12.23
 Author   : Ming-Chang Lee
 RWEPA    : http://rwepa.blogspot.tw/
 YouTube  : https://www.youtube.com/@alan9956
@@ -10,11 +9,6 @@ GitHub   : https://github.com/rwepa
 Encoding : UTF-8
 """
 
-# streamlit 執行方式
-# 考慮建立 D:\streamlitdata\hello_streamlit\app.py
-# 命令提示字元
-# d:
-# cd streamlitdata\hello_streamlit
 # streamlit run app.py
 # http://localhost:8501/
 
@@ -49,8 +43,8 @@ with st.sidebar:
     st.markdown('<p class="big-font">Hello Streamlit!</p>', unsafe_allow_html=True)
     # Add a slider to the sidebar:
     add_slider = st.sidebar.slider(
-        '**bins的個數**',
-        1, 50, 30
+        '**透明度**',
+        0.0, 1.0, 0.5
     )
     st.write("Required packages:")
     st.markdown('+ streamlit', unsafe_allow_html=True)
@@ -61,7 +55,7 @@ with st.sidebar:
 
 fig, ax = plt.subplots()
 # ax.hist(data, bins=add_slider)
-ax.scatter(df['eruptions'], df['waiting'], alpha=0.5) # 散佈圖
+ax.scatter(df['eruptions'], df['waiting'], alpha=add_slider) # 散佈圖
 ax.set_xlabel('eruptions(分鐘)')
 ax.set_ylabel('waiting(分鐘)')
 ax.set_title('eruptions vs. waiting 散佈圖 - 411410168李明昌')
