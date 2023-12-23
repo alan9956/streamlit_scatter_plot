24cb813a0b8c622b29ae5655965f6150fa0a5559
alan9956 <alan9956@ydu.edu.tw>
2023-12-23 12:56:33 +0800

    Update app.py

--- streamlit_scatter_plot/app.py
+++ streamlit_scatter_plot/app.py
@@ -19,6 +19,8 @@ import pandas as pd
 import matplotlib
 
 # 設定中文字型
+mpl.font_manager.fontManager.addfont('SimHei.ttf')
+
 # 設定 matplotlib.rcParams 方法
 matplotlib.rcParams['font.sans-serif'] = ['Microsoft YaHei']
 
